7e7fcc98cdae09985324cbdca9637d2683749bb4
Ahmed Ayman Elkhodary <126615749+aae121@users.noreply.github.com>
2025-11-10 23:17:45 +0200

    .

--- src/main.c
+++ src/main.c
@@ -60,7 +60,7 @@ int main(void) {
         fprintf(fp, "%ld,%.6f,%.6f,%.6f,%.6f\n", N, seq_t, pipes_t, mmap_t, threads_t);
         printf("Processed N=%ld\n", N);
 
-        N = (long)(N * 1.3); // Smooth increase (30% each step)
+        //N = (long)(N * 1.3); // Smooth increase (30% each step)
     }
 
     fclose(fp);
