f5f81a2dc8af580d9dae3adcd0f800a1967d38e8
Ahmed Ayman Elkhodary <126615749+aae121@users.noreply.github.com>
2025-11-10 16:46:32 +0200

    edited compute.h

--- include/compute.h
+++ include/compute.h
@@ -4,6 +4,7 @@
 unsigned long sequential_compute(const char *path, unsigned long (*func)(int, int));
 unsigned long pipes_compute(int n_proc, const char *path, unsigned long (*func)(int, int));
 unsigned long mmap_compute(int n_proc, const char *path, unsigned long (*func)(int, int));
-unsigned long threads_compute(int n_threads, const char *path, unsigned long (*func)(int, int));
+unsigned long threads_compute(int n_threads, const char *path, int (*func)(int, int));
+
 
 #endif // COMPUTE_H
