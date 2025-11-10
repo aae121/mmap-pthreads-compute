95796dc1245d35f0cdb12786a0c2b7758b7d6633
Ahmed Ayman Elkhodary <126615749+aae121@users.noreply.github.com>
2025-11-10 23:16:02 +0200

    .

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 
 #define DATA_PATH   "data/numbers.txt"
+#define N_PROC      16
+#define N_THREADS   16
 
 // Helper function to measure elapsed time
 static double elapsed(const struct timespec *a, const struct timespec *b) {
@@ -20,10 +22,10 @@ int main(void) {
 
     fprintf(fp, "N,Sequential,Pipes,Mmap,Threads\n");
 
-    long Number = 10000;  // Start small
-    while (Number <= 50000000000000000000000000000000000000000000) {  // Go up to 50 million
+    long N = 10000;  // Start small
+    while (N <= 5000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000) {  // Go up to 50 million
         char cmd[128];
-        snprintf(cmd, sizeof(cmd), "head -n %ld %s > temp.txt", (10000000000), DATA_PATH);
+        snprintf(cmd, sizeof(cmd), "head -n %ld %s > temp.txt", N, DATA_PATH);
         system(cmd);
 
         struct timespec t1, t2;
@@ -38,27 +40,27 @@ int main(void) {
 
         // Pipes
         clock_gettime(CLOCK_MONOTONIC, &t1);
-        pipes = pipes_compute(Number, "temp.txt", add_func);
+        pipes = pipes_compute(N_PROC, "temp.txt", add_func);
         clock_gettime(CLOCK_MONOTONIC, &t2);
         pipes_t = elapsed(&t1, &t2);
 
         // Mmap
         clock_gettime(CLOCK_MONOTONIC, &t1);
-        mm = mmap_compute(Number, "temp.txt", add_func);
+        mm = mmap_compute(N_PROC, "temp.txt", add_func);
         clock_gettime(CLOCK_MONOTONIC, &t2);
         mmap_t = elapsed(&t1, &t2);
 
         // Threads
         clock_gettime(CLOCK_MONOTONIC, &t1);
-        thr = threads_compute(Number, "temp.txt", add_func);
+        thr = threads_compute(N_THREADS, "temp.txt", add_func);
         clock_gettime(CLOCK_MONOTONIC, &t2);
         threads_t = elapsed(&t1, &t2);
 
         // Write to CSV
-        fprintf(fp, "%ld,%.6f,%.6f,%.6f,%.6f\n", Number, seq_t, pipes_t, mmap_t, threads_t);
-        printf("Processed number of processes=%ld\n", Number);
+        fprintf(fp, "%ld,%.6f,%.6f,%.6f,%.6f\n", N, seq_t, pipes_t, mmap_t, threads_t);
+        printf("Processed N=%ld\n", N);
 
-        Number = (long)(1.3*Number); // Smooth increase (30% each step)
+        N = (long)(N * 1.3); // Smooth increase (30% each step)
     }
 
     fclose(fp);
